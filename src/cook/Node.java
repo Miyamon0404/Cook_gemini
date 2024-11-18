6f0133b62537180584509fc313f027d3c7244979
Haruto Kajita <cnj19010@stn.nitech.ac.jp>
2024-11-18 13:20:25 +0900

    Nodeクラスを追加したやつ

---
+++ src/cook/Node.java
@@ -0,0 +1,39 @@
+package cook;
+import java.util.*;
+
+public class Node implements Comparable<Node> {
+    private String ingredientName;
+    private double quantityNeeded;
+    private double cost;
+    private Node parent;
+
+    public Node(String ingredientName, double quantityNeeded, double cost, Node parent) {
+        this.ingredientName = ingredientName;
+        this.quantityNeeded = quantityNeeded;
+        this.cost = cost;
+        this.parent = parent;
+    }
+
+    //ゲッター
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    public double getQuantityNeeded() {
+        return quantityNeeded;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    @Override
+    //ここで比較をするか否かは要検討必要
+    public int compareTo(Node other) {
+        return Double.compare(this.cost, other.cost);  // コスト順で優先度キューに追加
+    }
+}
